e1ea1d4da2da7ecda1bd7d5ef42d3f3ab83dd3b9
michael lester aguhob <amikell012@gmail.com>
2024-07-11 23:30:25 +0800

    socmed

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en"> 
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width-device-width, initial-scale=1.0">
+        <title>SocMed</title>
+        <link rel="stylesheet" href="../assets/css/bootstrap-5.3.3-dist/css/bootstrap.min.css">
+    </head>
+    <body class="bg-secondary">
+
+        <!-- <a href="includes/header.php">Header</a><br>  -->
+        <!-- <a href="pages/home.php">Home</a><br> -->
+        <!-- <a href="includes/footer.php">Footer</a> -->
+
+        <div class="container-fluid mt-5 border text-center" style="height: 80%;">
+            <div class="row">
+                <div class="col border align-items-end">
+                    <h1>LOGIN</h1>
+                    <div class="form-floating m-auto mt-5 mb-3" style="width: 80%;">
+                        <input type="email" id="InputEmail" class="form-control" placeholder="yourEmail@gmail.com">
+                        <label for="InputEmail">Email Address</label>
+                    </div>
+                    <div class="form-floating m-auto mt-5 mb-3" style="width: 80%;">
+                        <input type="password" id="InputPass" class="form-control" placeholder="Enter Password">
+                        <label for="InputPass">Password</label>
+                    </div>
+                </div>
+                <div class="col border">
+                    <h1>SOCMED</h1>
+                </div>
+                </div>
+        </div>
+
+        <script src="../assets/css/bootstrap-5.3.3-dist/js/bootstrap.min.js"> </script>
+    </body>
+</html>
