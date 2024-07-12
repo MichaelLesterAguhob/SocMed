d25bbd0985146de6ce4a778de3654bf95e85820a
michael lester aguhob <amikell012@gmail.com>
2024-07-13 01:28:35 +0800

    add footer

--- includes/footer.php
+++ includes/footer.php
@@ -3,8 +3,14 @@ class="container-fluid m-auto mt-5 border"
 style="position:relative; bottom: 0; height:200px;">
     
 <div class="row">
-    <div class="col border">col 1</div>
-    <div class="col border">col 2</div>
-    <div class="col border">col 3</div>
+    <div class="col-lg-4 p-3 border">
+        <h4>I am footer's header</h4>
+    </div>
+    <div class="col-lg-4 p-3 border">
+        <h4>I am footer's header</h4>
+    </div>
+    <div class="col-lg-4 p-3 border">
+        <h4>I am footer's header</h4>
+    </div>
 </div>
 </div>
