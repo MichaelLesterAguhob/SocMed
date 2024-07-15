33c359305f563a92df38682f60318f9d9513375a
michael lester aguhob <amikell012@gmail.com>
2024-07-15 23:33:26 +0800

    learning proper layout

--- includes/footer.php
+++ includes/footer.php
@@ -1,24 +1,28 @@
-<div 
-class="container-fluid m-auto mt-5 bg-secondary-subtle" 
-style="position:inherent; bottom: 0;">
-    
-<div class="row" style="min-height:200px;">
-    <div class="col-lg-4 p-3">
-        <h5 class="text-primary">Developer</h5>
-        <p>Micahel Lester Aguhob</p>
-       
-       
+<div class="container-fluid m-auto bg-secondary-subtle" style="position:inherent; bottom:0; height: 20vh; border:1px solid blue; ">
+    <div class="row" style="height: 100%;">
+
+        <div class="col-lg-4 p-3">
+            <h5 class="text-primary">Developer</h5>
+            <p>Micahel Lester Aguhob</p>
+        </div>
+
+        <div class="col-lg-4 p-3">
+            <h5 class="text-primary">About Us</h5>
+        </div>
+
+        <div class="col-lg-4 p-3">
+            <h5 class="text-primary">Follow us</h5>
+            <ul class="list-unstyled"> 
+                <li>Facebook</li>
+                <li>Instagram</li>
+                <li>Youtube</li>
+                <li>Github</li>
+            </ul>
+        </div>
+
     </div>
-    <div class="col-lg-4 p-3">
-        <h5 class="text-primary">I am footer's header</h5>
-      
-    </div>
-    <div class="col-lg-4 p-3">
-        <h5 class="text-primary">Follow us</h5>
-   
+    <div class="text-center bg-secondary-subtle" style="border: 1px solid green;">
+        <h6>SocMed Copyright © 2024 | All rights reserved. | Michael Lester Aguhob </h6>
     </div>
 </div>
-<div class="container text-center p-2">
-    <h6>SocMed Copyright © 2024 | All rights reserved. | Michael Lester Aguhob </h6>
-</div>
-</div>
+
--- index.php
+++ index.php
@@ -1,89 +1,61 @@
 <!DOCTYPE html>
-<html lang="en"> 
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>SocMed</title>
-        <link rel="stylesheet" href="../assets/css/bootstrap-5.3.3-dist/css/bootstrap.min.css">
-    </head>
-    <body class="bg-light">
+<html lang="en">
 
-        <!-- <?php include_once("includes/header.php")?> -->
-        <!-- <a href="pages/home.php">Home</a><br> -->
-        <!-- <a href="includes/footer.php">Footer</a> -->
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no"> -->
+    <title>SocMed</title>
+    <link rel="stylesheet" href="../assets/css/bootstrap-5.3.3-dist/css/bootstrap.min.css">
+</head>
 
-        <div class="container-fluid">
-            <div class="row mt-5">
-                <!-- LOGO -->
-                    <div class="col-12 col-lg-6 mb-5">
-                        <div class="m-auto align-items-center" style="height: 70%; width: 80%;">
-                            <img src="assets/image/logo.png" alt="SocMed Logo" class="img-fluid mt-5">
-                        </div>
-                    </div>
-                    
-                <!-- LOGIN -->
-                    <div id="LoginForm" class="col-12 col-lg-6 text-center">
-                        <div class="m-auto border" style="width: 90%; padding-bottom: 100px;"">
-                              <h1 class="text-primary mt-5">Login</h1>
-                            <div class="form-floating m-auto mt-5 mb-3" style="width: 80%;">
-                                <input type="email" id="InputEmail" class="form-control" placeholder="yourEmail@gmail.com">
-                                <label for="InputEmail">Email Address</label>
-                            </div>
-                            <div class="form-floating m-auto mb-3" style="width: 80%;">
-                                <input type="password" id="InputPass" class="form-control" placeholder="Enter Password">
-                                <label for="InputPass">Password</label>
+<body style="height: 100vh; display:flex; align-items:center;">
 
-                                <div class="d-flex justify-content-between align-items-center">
-                                    <button class="btn " style="font-size: small;">Show password</button>
-                                    <a href="#" style="font-size: small;">Forgot password?</a>
-                                </div>
-                            </div>
-                            <div class="d-grid gap-2 mt-5 d-inline-flex text-center" style="width: 80%;">
-                                <button id="BtnLogin" class="btn btn-primary btn-md m-auto" style="width: 50%;">Login</button>
-                                <button id="BtnGoSignup" class="btn btn-warning btn-md m-auto" style="width: 50%;" onclick="goToSignUp()">Signup →</button>
-                            
-                            </div>
-                        </div>
-                    </div>
-
-                    <!-- SIGNUP -->
-                    <div id="SignupForm" class="col-12 col-lg-6 text-center" style="display: none;">
-                        <div class="m-auto border" style="width: 90%; padding-bottom: 100px;">
-                              <h1 class="text-primary mt-5">Signup</h1>
-                            <div class="form-floating m-auto mt-5 mb-3" style="width: 80%;">
-                                <input type="email" id="InputCreateEmail" class="form-control" placeholder="yourEmail@gmail.com">
-                                <label for="InputCreateEmail">Enter Email</label>
-                            </div>
-                            <div class="form-floating m-auto mb-3" style="width: 80%;">
-                                <input type="password" id="InputCreatePass" class="inputPass form-control" placeholder="Create Password">
-                                <label for="InputCreatePass">Create Password</label>                        
-                            </div>
-                            <div class="form-floating m-auto mb-3" style="width: 80%;">
-                                <input type="password" id="InputComfirmPass" class="inputPass form-control" placeholder="Confirm Password">
-                                <label for="InputComfirmPass">Confirm Password</label>
-
-                                <div class="d-flex justify-content-between align-items-center">
-                                    <button id="BtnShowHide" class="btn" style="font-size: small;" onclick="showHideSignupPass()">Show password</button>
-                                </div>
-                            </div>
-                            <div class="d-grid gap-2 mt-5 d-inline-flex text-center" style="width: 80%;">
-                                <button id="BtnSignup" class="btn btn-warning btn-md m-auto" style="width: 50%;" onclick="">Signup</button>
-                                <button id="BtnGoLogin" class="btn btn-primary btn-md m-auto" style="width: 50%;" onclick="goToLogIn()">← Login</button>
-                  
-                            </div>
-                        </div>
-                    </div>
-            </div>
-
-        </div>
+    <!-- HEADER/ NAVIGATION BAR -->
 
 
 
 
+    <!-- MAIN CONTENT -->
 
-        <?php include_once("includes/footer.php") ?>
-        <!-- JS -->
-        <script src="../assets/css/bootstrap-5.3.3-dist/js/bootstrap.min.js"> </script>
-        <script src="assets/js/index.js"></script>
-    </body>
+    <div style="margin: auto; border: 1px solid black; height:40%; width:40%; display:flex; align-items:center; flex-direction:column;">
+        <h6>String input to Array TRY</h6>
+        <input type="number" id="input_num" style="margin:auto; width:80%;">
+        <button id="btn" style="margin: auto;">Solve</button>
+        <p id="res">result</p>
+    </div>
+
+    <script>
+        document.getElementById("btn").addEventListener("click", function() {
+            let array_nums = [];
+            let numbers_entered = document.getElementById("input_num").value;
+            let cntr = numbers_entered.length;
+            let i = 0;
+            while(cntr != 0){
+                array_nums.push(numbers_entered[i])
+                i++;
+                cntr--;
+            };
+
+            
+       
+            document.getElementById("res").innerHTML = "Input converted to array = " + array_nums;
+            alert(i + cntr);
+           
+
+            // document.getElementById("res").innerHTML = array_nums;
+        });
+    </script>
+
+
+    <!-- FOOTER -->
+
+
+
+
+    <!-- JAVASCRIPT -->
+    <!-- <script src="../assets/css/bootstrap-5.3.3-dist/js/bootstrap.min.js"> </script> -->
+    <!-- <script src="assets/js/index.js"></script> -->
+</body>
+
 </html>
